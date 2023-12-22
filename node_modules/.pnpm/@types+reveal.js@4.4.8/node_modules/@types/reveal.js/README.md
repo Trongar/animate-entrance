bddb21f121c5a3235b540070c6ce2ecda937ab7e
Trongar <sanchesedgar97@gmail.com>
2023-12-22 13:59:41 -0600

    first commit

---
+++ node_modules/.pnpm/@types+reveal.js@4.4.8/node_modules/@types/reveal.js/README.md
@@ -0,0 +1,15 @@
+# Installation
+> `npm install --save @types/reveal.js`
+
+# Summary
+This package contains type definitions for reveal.js (https://github.com/hakimel/reveal.js/).
+
+# Details
+Files were exported from https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/reveal.js.
+
+### Additional Details
+ * Last updated: Wed, 22 Nov 2023 01:32:11 GMT
+ * Dependencies: none
+
+# Credits
+These definitions were written by [robertop87](https://github.com/robertop87), [Nava2](https://github.com/Nava2), [JPtenBerge](https://github.com/JPtenBerge), and [Keita Watanabe](https://github.com/kwatanwa17).
---
+++ src/slides.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+	import { Presentation, Slide } from '@components'
+	import { onMount } from 'svelte'
+	import { initializeApp } from 'firebase/app'
+	import { getDatabase, ref, onValue } from 'firebase/database'
+
+	const firebaseConfig = {
+		apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+		authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+		databaseURL: import.meta.env.VITE_APP_FIREBASE_DATABASE_URL,
+		projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+		storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+		messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+		appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+	}
+
+	// Initialize Firebase
+	const app = initializeApp(firebaseConfig)
+	const db = getDatabase(app)
+
+	type firebaseData = {
+		[key: string]: {
+			email: string
+			name: string
+			sound: string
+		}
+	}
+	type firebaseDataReg = Array<firebaseData[keyof firebaseData]>
+
+	let people: firebaseDataReg = []
+
+	let peopleCount = 0
+	const audioPlayer = new Audio()
+
+	const runAnnouncer = (sound: string) => {
+		audioPlayer.pause()
+		audioPlayer.src = sound
+		audioPlayer.play()
+	}
+
+	// Escuchar cambios en el valor de la slide actual en Firebase
+	onMount(() => {
+		const slideRef = ref(db, 'usersRegistered')
+
+		onValue(slideRef, (snapshot) => {
+			const data: firebaseData = snapshot.val()
+			if (data) {
+				people = Object.values(data)
+				peopleCount = people.length
+			}
+		})
+	})
+</script>
+
+{#if !(peopleCount > 0)}
+	<Presentation>
+		<Slide animate>
+			<p class="font-bold text-8xl">No hay nadie en la sala</p>
+		</Slide>
+	</Presentation>
+{:else}
+	<Presentation>
+		<Slide animate>
+			<p class="font-bold text-8xl">Listo!</p>
+		</Slide>
+		{#each people as person}
+			<Slide
+				video="https://download-video.akamaized.net/v3-1/download/5151fb41-6bd9-4d60-8658-41d1783c18a8/ebd0bd5b/cGV4ZWxzLW1hdGhldXMtZGUtbW9yYWVzLWd1Z2VsbWltLTE1NzEzMzQwICg3MjBwKS5tcDQ?__token__=st=1703274690~exp=1703361210~acl=%2Fv3-1%2Fdownload%2F5151fb41-6bd9-4d60-8658-41d1783c18a8%2Febd0bd5b%2FcGV4ZWxzLW1hdGhldXMtZGUtbW9yYWVzLWd1Z2VsbWltLTE1NzEzMzQwICg3MjBwKS5tcDQ%2A~hmac=3a449497b3d75480a4d28f6678939e7711ab5e778a3f93276ed8f76f447a3bed&r=dXMtZWFzdDE%3D"
+				animate
+				on:in={() => runAnnouncer(person.sound)}
+			>
+				<p class="font-bold text-8xl">Bienvenido {person.name}</p>
+			</Slide>
+		{/each}
+	</Presentation>
+{/if}
